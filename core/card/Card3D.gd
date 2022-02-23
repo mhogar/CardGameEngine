725f0eb4ab6cd651eccf61c066d143634b178804
Ryan <mhogar98@gmail.com>
2022-02-22 22:49:19 -0500

    Add front texture logic based on suit and value

--- core/card/Card3D.gd
+++ core/card/Card3D.gd
@@ -3,9 +3,20 @@ class_name Card3D
 
 signal flip_finished
 
+export(int, 12) var value : int
+export(int, 3) var suit : int
+
 onready var anim_player : AnimationPlayer = $AnimationPlayer
+onready var mesh : MeshInstance = $Card
+
+const NUM_VALUES := 13
+const NUM_SUITS := 4
 
 var face_up := true
+
+
+func _ready():
+	assign_front_texture(value, suit)
 
 
 func flip():
@@ -13,6 +24,10 @@ func flip():
 		anim_player.play("Flip")
 	else:
 		anim_player.play_backwards("Flip")
+
+
+func assign_front_texture(col : int, row : int):
+	mesh["material/0"].uv1_offset = Vector3(float(col) / NUM_VALUES, float(row) / NUM_SUITS, 1.0)
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
