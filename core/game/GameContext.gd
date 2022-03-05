5b004ec39d3f79c183d93d520607256b31cdd305
Ryan <mhogar98@gmail.com>
2022-03-05 18:50:13 -0500

    Fix bug with removing players

--- core/game/GameContext.gd
+++ core/game/GameContext.gd
@@ -14,21 +14,30 @@ func _init():
 	scoreboard = ScoreboardData.new()
 
 
-func get_relative_player_index(index : int) -> int:
-	return (current_player_index + index) % players.size()
+func get_relative_player_index(relative_index : int) -> int:
+	return (current_player_index + relative_index) % players.size()
 
 
-func get_relative_player(index : int) -> Player:
-	return players[get_relative_player_index(index)]
+func get_relative_player(relative_index : int) -> Player:
+	return players[get_relative_player_index(relative_index)]
 	
 
+func set_current_player_index(relative_index : int):
+	current_player_index = (current_player_index + relative_index) % players.size()
+
+
 func next_turn():
-	current_player_index = (current_player_index + turn_dir) % players.size()
+	set_current_player_index(turn_dir)
 
 
-func remove_player(index : int) -> Player:
-	var player : Player = get_relative_player(index)
+func remove_player(relative_index : int) -> Player:
+	var player_index := get_relative_player_index(relative_index)
+	var player : Player = players[player_index]
 	players.erase(player)
+
+	if current_player_index >= player_index:	
+		set_current_player_index(-1)
+	
 	return player
 	
 
