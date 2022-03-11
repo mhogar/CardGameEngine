71220db7a314be7f97fc5a6e933eed2025192e75
Ryan <mhogar98@gmail.com>
2022-03-11 13:48:36 -0500

    Fix bug where MoveCards fires completed event to early

--- core/events/card/MoveCards.gd
+++ core/events/card/MoveCards.gd
@@ -6,19 +6,21 @@ onready var tween := $Tween
 
 func execute(ctx : GameContext, inputs : Dictionary):
 	var deck : Deck = inputs["source_deck"]
+	var last_index := deck.num_cards() - 2
 	
 	var delay := 0.0
-	for _index in range(deck.num_cards() - 1):
-		var event := preload("res://core/events/card/MoveCard.tscn").instance()
+	for index in range(last_index + 1):
+		var event : Event = preload("res://core/events/card/MoveCard.tscn").instance()
 		add_child(event)
 		
 		tween.interpolate_deferred_callback(event, delay, "execute", ctx, merge_inputs(inputs, { "card_index": 0 }))
 		delay += 0.1
+		
+		if index == last_index:
+			event.connect("completed", self, "_on_last_event_completed", [], CONNECT_DEFERRED | CONNECT_ONESHOT)
 	
-	tween.connect("tween_all_completed", self, "_on_Tween_tween_all_completed", [], CONNECT_DEFERRED | CONNECT_ONESHOT)
 	tween.start()
 
 
-func _on_Tween_tween_all_completed():
-	tween.remove_all()
+func _on_last_event_completed(_event : Event, _outputs : Dictionary):
 	emit_signal("completed", self, {})
